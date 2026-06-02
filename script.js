a1fcf49642fcd91e8343cac1728de82240f92afa
winbiger <fortune882025@outlook.com>
2026-06-02 20:12:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24634,6 +24634,33 @@ document.addEventListener("DOMContentLoaded", function () {
                     { "name": "轮渡往返2.8+1.8", "price": 4.60 }
                 ]
             }
+        ],
+        "2026-06-02": [
+            {
+                "shop": "支",
+                "note": "上海公共交通卡股份有限公司",
+                "items": [
+                    { "name": "上海地铁-康恒路⇌宁国路", "price": 8.00 }
+                ]
+            }, {
+                "shop": "微信",
+                "note": "上海宜家家居有限公司浦东分公司北蔡商场",
+                "items": [
+                    { "name": "宜家兼职员工餐 晚餐", "price": 10.00 }
+                ]
+            }, {
+                "shop": "山姆",
+                "note": "山姆会员店(上海浦东店)",
+                "items": [
+                    { "name": "有机香蕉1.2kg", "price": 22.90 },
+                    { "name": "鲜毛豆1.1kg", "price": 17.90 },
+                    { "name": "有机胡萝卜1kg", "price": 11.90 },
+                    { "name": "有机奶白菜苗600g", "price": 14.90 },
+                    { "name": "香菇320g", "price": 13.90 },
+                    { "name": "精选鲜鸡蛋1.59kg(30枚)", "price": 18.90 },
+                    { "name": "包装费", "price": 1.00 }
+                ]
+            }
         ]
 
     };
